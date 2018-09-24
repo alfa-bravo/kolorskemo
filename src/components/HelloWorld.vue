5b2b3e7650ff9549e05b85e0e38ee8fec92b2e37
stephenkairos <stephenkairos.reyes@sjsu.edu>
2018-09-24 14:03:57 -0700

    Basic Changes
    Testing hot reload, README edits

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>Hot Reload is Really Cool</p>
     <p>
       For guide and recipes on how to configure / customize this project,<br>
       check out the
